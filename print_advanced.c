21ee40947a0b298c15072129d5293ef93d05e137
AfnanAljohani <13258@holbertonstudents.com>
2026-04-17 16:17:30 +0000

    Fix: Add missing print_pointer function

--- print_advanced.c
+++ print_advanced.c
@@ -36,3 +36,41 @@ int print_S(va_list args)
 	}
 	return (count);
 }
+
+/**
+ * print_pointer - prints pointer address in hex
+ * @args: va_list containing the pointer
+ * Return: number of characters printed
+ */
+int print_pointer(va_list args)
+{
+	unsigned long int p;
+	int count;
+	int i;
+	char digits[20];
+	char *hex = "0123456789abcdef";
+
+	p = va_arg(args, unsigned long int);
+	if (p == 0)
+	{
+		write(1, "(nil)", 5);
+		return (5);
+	}
+	write(1, "0x", 2);
+	count = 2;
+	i = 0;
+	while (p > 0)
+	{
+		digits[i] = hex[p % 16];
+		p /= 16;
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(digits[i]);
+		count++;
+		i--;
+	}
+	return (count);
+}
